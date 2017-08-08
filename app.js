cd084cf70bfd8a4d5a8dcfd3d331b17d8a080f4a
rasg <rodolfo@mutalis.com>
2017-08-08 13:27:16 -0500

    Add a Toggle All Complete Button with React Native TouchableOpacity

--- app.js
+++ app.js
@@ -8,10 +8,25 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      allComplete: false,
       value: '',
       items: []
     }
     this.handleAddItem = this.handleAddItem.bind(this);
+    this.handleToggleAllComplete = this.handleToggleAllComplete.bind(this);
+  }
+
+  handleToggleAllComplete() {
+    const complete = !this.state.allComplete;
+    const newItems = this.state.items.map((item) => ({
+      ... item,
+      complete
+    }))
+    console.table(newItems);
+    this.setState({
+      items: newItems,
+      allComplete: complete
+    })
   }
 
   handleAddItem() {
@@ -37,6 +52,7 @@ class App extends Component {
           value={this.state.value}
           onAddItem={this.handleAddItem}
           onChange={(value) => this.setState({value})}
+          onToggleAllComplete={this.handleToggleAllComplete}
         />
         <View style={styles.content}>
         </View>
